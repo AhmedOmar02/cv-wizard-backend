cbf6b58d0f797dd81cf38331709c1dd8f631bda1
AhmedOmar02 <ahmedalimrany@gmail.com>
2025-08-12 12:24:01 +0300

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('CV Wizard Backend is running!');
+});
+
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
