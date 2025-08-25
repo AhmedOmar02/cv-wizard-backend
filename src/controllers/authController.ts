1d45495644133f20029a69795b392a9d2ec9492f
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-08-25 21:00:43 +0200

    Some modifications

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,15 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
 import { catchAsync } from '../utils/catchAsync';
 import APIError from '../classes/APIError';
 import prisma from '../db/client';
 import { SUCCESS } from '../constants/responseConstants';
+import { Role } from '@prisma/client';
 
+const { REGULAR } = Role;
 
-export const register = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const { email, password, firstName, lastName, phoneNumber, city, country, linkedinUrl, role } = req.body;
+export const register = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
+  const { email,
+    password,
+    firstName,
+    lastName,
+    phoneNumber,
+    city,
+    country,
+    linkedinUrl,
+  } = req.body;
 
   // Check for existing email
   const existing = await prisma.user.findUnique({ where: { email } });
@@ -18,8 +27,6 @@ export const register = catchAsync(async (req: Request, res: Response, next: Nex
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
-
-  //const confirmationCode = crypto.randomBytes(8).toString('hex');
 
   const user = await prisma.user.create({
     data: {
@@ -31,7 +38,7 @@ export const register = catchAsync(async (req: Request, res: Response, next: Nex
       city,
       country,
       linkedinUrl,
-      role,
+      role: REGULAR,
       isVerified: true,
     },
   });
@@ -41,10 +48,9 @@ export const register = catchAsync(async (req: Request, res: Response, next: Nex
       sub: user.id,
       role: user.role,
       iss: process.env.JWT_ISS,
-      partial: true,
     },
     process.env.JWT_SECRET as string,
-    { expiresIn: '1h' }
+    { expiresIn: '1d' },
   );
 
   res.cookie('token', token, {
@@ -52,11 +58,11 @@ export const register = catchAsync(async (req: Request, res: Response, next: Nex
     httpOnly: false,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
-    maxAge: 60 * 60 * 1000,
+    maxAge: 24 * 60 * 60 * 1000,
   });
 
   res.status(201).json({
     status: SUCCESS,
-    message: `Registered successfully, a verification link has been sent to ${email}`,
+    message: 'User Registered successfully',
   });
 });
