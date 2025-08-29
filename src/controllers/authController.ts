eb82cd9f52584d8409b8bdc65835e73c0c975f71
AhmedOmar02 <ahmedalimrany@gmail.com>
2025-08-29 10:51:18 +0300

    login endpoint added and tested

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -66,3 +66,46 @@ export const register = catchAsync(async(req: Request, res: Response, next: Next
     message: 'User Registered successfully',
   });
 });
+
+export const login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id, email, password } = req.body;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      OR: [{ id }, { email }],
+    },
+  });
+
+  if (!user) {
+    return next(new APIError(401, 'Invalid id/email or password'));
+  }
+
+  const validPassword = await bcrypt.compare(password, user.passwordHash);
+  if (!validPassword) {
+    return next(new APIError(401, 'Invalid id/email or password'));
+  }
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      role: user.role,
+      iss: process.env.JWT_ISS,
+    },
+    process.env.JWT_SECRET as string,
+    { expiresIn: '1d' }
+  );
+
+  res.cookie('token', token, {
+    path: '/',
+    httpOnly: true, // keep true for security
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  });
+
+  res.status(200).json({
+    status: SUCCESS,
+    message: 'Successfully logged in.',
+    token,
+  });
+});
